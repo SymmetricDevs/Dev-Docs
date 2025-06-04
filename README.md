b0c2569a938184f74a93a1074dde23c962f773d4
Brandon Li <48413902+aspiringLich@users.noreply.github.com>
2025-06-04 00:33:15 -0400

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# SUSY Dev Docs
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            mdbook
+            mdbook-mermaid
+            mdbook-linkcheck
+            mdbook-admonish
+            mdbook-emojicodes
+          ];
+        };
+      }
+    );
+}
